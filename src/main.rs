9909600cc11ba269e41319d36065c5bf2fdf381b
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-08 20:51:32 -0400

    Add input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Echo!");
+
+    loop {
+        let mut message = String::new();
+        io::stdin().read_line(&mut message).expect("Failed to read line");
+
+        println!("Echo: {}", message)
+    }
 }
